9e41e07f3e58e8480b6b2b70dbc114e4f1be7465
Sai Rachaveti <sai.rachaveti2@t-mobile.com>
2026-09-10 19:55:42 -0700

    Enable AWS WAF in count mode

--- terraform/waf.tf
+++ terraform/waf.tf
@@ -13,7 +13,7 @@ resource "aws_wafv2_web_acl" "main" {
     priority = 10
 
     override_action {
-      none {}
+      count {}
     }
 
     statement {
@@ -35,7 +35,7 @@ resource "aws_wafv2_web_acl" "main" {
     priority = 20
 
     override_action {
-      none {}
+      count {}
     }
 
     statement {
@@ -57,7 +57,7 @@ resource "aws_wafv2_web_acl" "main" {
     priority = 30
 
     override_action {
-      none {}
+      count {}
     }
 
     statement {
@@ -79,7 +79,7 @@ resource "aws_wafv2_web_acl" "main" {
     priority = 40
 
     action {
-      block {}
+      count {}
     }
 
     statement {
